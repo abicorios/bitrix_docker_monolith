a3015e3f8293935b4e6d229a96787ce41390c660
abicorios <yaroslav.moroz91@gmail.com>
2020-04-11 18:28:54 +0300

    add create_bitrix

---
+++ create_bitrix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo mkdir www
+mkdir www
+echo docker build --rm --no-cache -t c7 .
+time docker build --rm --no-cache -t c7 .
+echo docker run --privileged --name c7 -p 80:80 -p 443:443 -p 9000:9000 -p 3306:3306 -p 11211:11211 -v $PWD/www:/home/bitrix/www -d c7
+time docker run --privileged --name c7 -p 80:80 -p 443:443 -p 9000:9000 -p 3306:3306 -p 11211:11211 -v $PWD/www:/home/bitrix/www -d c7
+echo docker exec c7 umount /home/bitrix/www
+docker exec c7 umount /home/bitrix/www
+echo docker exec c7 rm -rf /home/bitrix/www
+docker exec c7 rm -rf /home/bitrix/www
+echo docker exec c7 /root/bitrix-env.sh
+time docker exec c7 /root/bitrix-env.sh
+echo docker exec c7 mv /home/bitrix/www{,.bac}
+docker exec c7 mv /home/bitrix/www{,.bac}
+echo docker container restart c7
+time docker container restart c7
+echo docker exec c7 rsync -az /home/bitrix/www.bac/ /home/bitrix/www/
+docker exec c7 rsync -az /home/bitrix/www.bac/ /home/bitrix/www/
