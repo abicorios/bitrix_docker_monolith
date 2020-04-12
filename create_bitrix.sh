95410d318610c6f7909174dff339dac03b928a97
abicorios <yaroslav.moroz91@gmail.com>
2020-04-12 15:37:56 +0300

    use get_ip and open ip

--- create_bitrix.sh
+++ create_bitrix.sh
@@ -30,5 +30,5 @@ echo docker container restart $project
 time docker container restart $project
 echo docker exec $project rsync -az /home/bitrix/www.bac/ /home/bitrix/www/
 docker exec $project rsync -az /home/bitrix/www.bac/ /home/bitrix/www/
-ip=`docker exec $project hostname -I`
-echo "ip: $ip"
+. get_ip.sh
+xdg-open http://$ip>/dev/null 2>&1
