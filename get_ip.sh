98014e2b91d0bb315688879098af41b5685326a5
abicorios <yaroslav.moroz91@gmail.com>
2020-04-12 15:36:29 +0300

    add get_ip

---
+++ get_ip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source config
+if [[ `docker ps -f status=running -f name=$project | wc -l` == 2 ]]
+then
+	ip=`docker exec $project hostname -I`
+	echo ip: $ip
+else
+	echo container $project is not running
+fi
